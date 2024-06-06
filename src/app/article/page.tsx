c081f2ed0b79e7eb559f8baca00c81a429b18532
soonduck <soonduck3729@gmail.com>
2024-06-06 15:25:14 +0900

    Refactor: 코드 리팩토링

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import { useState } from 'react';
 import {
   Button,
   Image,
@@ -17,11 +17,53 @@ import {
   useDisclosure,
   Input,
   Textarea,
+  Checkbox,
 } from '@nextui-org/react';
 
 export default function Article() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const modalPlacement = 'center';
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [checkedFiles, setCheckedFiles] = useState<boolean[]>([]);
+  const [activeButton, setActiveButton] = useState('');
+
+  const handleButtonClick = (buttonName: string) => {
+    setActiveButton(buttonName);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filesList = e.target.files;
+    if (!filesList) return; // 파일이 선택되지 않은 경우
+
+    const filesArray = Array.from(filesList);
+    setSelectedFiles((prevFiles: File[]) => [...prevFiles, ...filesArray]);
+    setCheckedFiles((prevChecked: boolean[]) => [
+      ...prevChecked,
+      ...filesArray.map(() => false),
+    ]);
+  };
+
+  const handleCheckboxChange = (index: number) => {
+    setCheckedFiles((prevChecked) => {
+      const newChecked = [...prevChecked];
+      newChecked[index] = !newChecked[index];
+      return newChecked;
+    });
+  };
+
+  const handleDeleteSelectedFiles = () => {
+    const newSelectedFiles = selectedFiles.filter(
+      (_, index) => !checkedFiles[index]
+    );
+    const newCheckedFiles = checkedFiles.filter((checked) => !checked);
+    setSelectedFiles(newSelectedFiles);
+    setCheckedFiles(newCheckedFiles);
+  };
+
+  const handleDeleteAllFiles = () => {
+    setSelectedFiles([]);
+    setCheckedFiles([]);
+  };
 
   return (
     <div className="bg-white h-screen mx-auto">
@@ -73,25 +115,95 @@ export default function Article() {
                 게시글 작성
               </ModalHeader>
               <ModalBody>
+                <div className="flex gap-2">
+                  <Button
+                    color="primary"
+                    variant={activeButton === '자유' ? 'solid' : 'ghost'}
+                    onClick={() => handleButtonClick('자유')}
+                  >
+                    자유
+                  </Button>
+                  <Button
+                    color="primary"
+                    variant={activeButton === '정보' ? 'solid' : 'ghost'}
+                    onClick={() => handleButtonClick('정보')}
+                  >
+                    정보
+                  </Button>
+                  <Button
+                    color="primary"
+                    variant={activeButton === '공략' ? 'solid' : 'ghost'}
+                    onClick={() => handleButtonClick('공략')}
+                  >
+                    공략
+                  </Button>
+                  <Button
+                    color="primary"
+                    variant={activeButton === '리뷰' ? 'solid' : 'ghost'}
+                    onClick={() => handleButtonClick('리뷰')}
+                  >
+                    리뷰
+                  </Button>
+                </div>
                 <p>게임 검색</p>
-                <Input
-                  placeholder="게임을 검색해주세요."
-                  width="100%"
-                  className="mb-4"
-                />
+                <Input type="text" label="게임을 검색해주세요." />
                 <hr style={{ border: '1px solid #ddd' }} />
                 <p>제목</p>
-                <Input
-                  placeholder="제목을 입력해주세요."
-                  width="100%"
-                  className="mb-4"
-                />
+                <Input placeholder="제목을 입력해주세요." className="mb-4" />
                 <p>내용</p>
-                <Textarea
-                  placeholder="내용을 입력해주세요."
-                  width="100%"
-                  className="mb-4"
+                <Textarea placeholder="내용을 입력해주세요." className="mb-4" />
+                <hr style={{ border: '1px solid #ddd' }} />
+                {/*  파일 업로드  */}
+                <div
+                  style={{ display: 'flex', gap: '10px', alignItems: 'center' }}
+                >
+                  <label
+                    htmlFor="fileUpload"
+                    style={{
+                      cursor: 'pointer',
+                      maxWidth: '81px',
+                      border: '1px solid #ccc',
+                      padding: '5px',
+                      borderRadius: '5px',
+                    }}
+                  >
+                    파일 선택
+                  </label>
+                  <Button
+                    color="danger"
+                    variant="light"
+                    onClick={handleDeleteAllFiles}
+                  >
+                    파일 전체 삭제
+                  </Button>
+                  <Button
+                    color="danger"
+                    variant="light"
+                    onClick={handleDeleteSelectedFiles}
+                  >
+                    선택된 파일 삭제
+                  </Button>
+                </div>
+                <input
+                  type="file"
+                  multiple
+                  id="fileUpload"
+                  onChange={handleFileChange}
+                  style={{ display: 'none' }}
                 />
+                {selectedFiles.map((file) => (
+                  <div key={file.name}>
+                    <label>
+                      <Checkbox
+                        checked={!!checkedFiles[selectedFiles.indexOf(file)]}
+                        onChange={() =>
+                          handleCheckboxChange(selectedFiles.indexOf(file))
+                        }
+                      />
+                      <span>{file.name}</span>
+                    </label>
+                  </div>
+                ))}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
