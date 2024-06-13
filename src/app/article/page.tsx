46c98f9cd4727d0c228bd1d4d946f910026420d2
soonduck <soonduck3729@gmail.com>
2024-06-13 15:28:03 +0900

    fix: CHATTING 수정

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -270,7 +270,7 @@ export default function Article() {
           return 'bg-yellow-500';
         case 'REVIEW':
           return 'bg-red-500';
-        case 'CHATING':
+        case 'CHATTING':
           return 'bg-black';
         default:
           return 'bg-gray-500';
@@ -326,8 +326,8 @@ export default function Article() {
               </Button>
               <Button
                   color="primary"
-                  variant={activeButton === 'CHATING' ? 'solid' : 'ghost'}
-                  onClick={() => handleClick('CHATING')}
+                  variant={activeButton === 'CHATTING' ? 'solid' : 'ghost'}
+                  onClick={() => handleClick('CHATTING')}
               >
                 채팅
               </Button>
@@ -392,8 +392,8 @@ export default function Article() {
                         </Button>
                         <Button
                             color="primary"
-                            variant={activeButton === 'CHATING' ? 'solid' : 'ghost'}
-                            onClick={() => handleButtonClick('CHATING')}
+                            variant={activeButton === 'CHATTING' ? 'solid' : 'ghost'}
+                            onClick={() => handleButtonClick('CHATTING')}
                         >
                           채팅
                         </Button>
